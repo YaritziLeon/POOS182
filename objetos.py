691fdeafaf597adc5488bd77f4f4c81c2aefe2a8
YaritziLeon <yaritzileonm08@gmail.com>
2023-02-20 15:44:23 -0600

    Se creo el método lanzar granadas y recargar armas

---
+++ objetos.py
@@ -0,0 +1,9 @@
+from personaje import *
+
+#Creamos una instancia de la clase Personaje que se llama Heroe
+Heroe= Personaje()
+
+#usamos los atributos
+print("El personaje se llama: "+Heroe.nombre)
+print("Pertenece a la especie: "+Heroe.especie)
+print("Y una altura de: "+str(Heroe.altura))
--- personaje.py
+++ personaje.py
@@ -7,6 +7,14 @@ class Personaje:
     #metodos
     def correr(self,status):
         if(status):
-            print("El personaje "+self.nombre+"esta corriendo")
+            print("El personaje "+self.nombre+" esta corriendo")
         else:
-            print("El personaje "+self.nombre+"se detuvo")
+            print("El personaje "+self.nombre+" se detuvo")
+
+    def lanzarGranadas(self):
+        print("El personaje "+self.nombre+" lanzo granada")
+    
+    def recargarArma(self,municiones):
+        cargador=10
+        cargador=cargador+municiones
+        print("El arma recargada tiene "+cargador+" balas")
