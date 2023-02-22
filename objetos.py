8d7a61e1438fb9ac65a4b1bcb163337894a8184e
YaritziLeon <yaritzileonm08@gmail.com>
2023-02-22 15:34:09 -0600

    Ejemplificación del constructor

--- objetos.py
+++ objetos.py
@@ -1,9 +1,42 @@
 from personaje import *
 
-#Creamos una instancia de la clase Personaje que se llama Heroe
-Heroe= Personaje()
+#1. Solicitamos los datos
+print("")
+print("DATOS HEROE: ")
+especieH=input("Escribe la especie del Heroe: ")
+nombreH=input("Escribe el nombre del Heroe: ")
+alturaH=float(input("Escribe la altura del Heroe: "))
+recargaH=int(input("Ingresa las balas para el Heroe: "))
 
-#usamos los atributos
+print("")
+print("DATOS VILLANO: ")
+especieV=input("Escribe la especie del Villano: ")
+nombreV=input("Escribe el nombre del Villano: ")
+alturaV=float(input("Escribe la altura del Villano: "))
+recargaV=int(input("Ingresa las balas para el Villano: "))
+
+#2. Creamos los objetos
+Heroe=Personaje(especieH, nombreH, alturaH)
+Villano=Personaje(especieV, nombreV, alturaV)
+
+#3. usamos los atributos Heroe y Villano
+
+print("")
+print("Metodos y Atributos del Heroe: ")
 print("El personaje se llama: "+Heroe.nombre)
 print("Pertenece a la especie: "+Heroe.especie)
 print("Y una altura de: "+str(Heroe.altura))
+
+Heroe.correr(True)
+Heroe.lanzarGranadas()
+Heroe.recargarArma(37)
+
+print("")
+print("Metodos y Atributos del VILLANO: ")
+print("El personaje se llama: "+Villano.nombre)
+print("Pertenece a la especie: "+Villano.especie)
+print("Y una altura de: "+str(Villano.altura))
+
+Villano.correr(True)
+Villano.lanzarGranadas()
+Villano.recargarArma(37)
--- personaje.py
+++ personaje.py
@@ -1,8 +1,11 @@
 class Personaje:
+    
+    #Agregamos el constructor del personaje
+    def __init__(self,esp,nom,alt):
     #atributos
-    especie="Hunamano"
-    nombre="MasterChief"
-    altura=2.70
+        self.especie=esp
+        self.nombre=nom
+        self.altura=alt
     
     #metodos
     def correr(self,status):
@@ -17,4 +20,4 @@ class Personaje:
     def recargarArma(self,municiones):
         cargador=10
         cargador=cargador+municiones
-        print("El arma recargada tiene "+cargador+" balas")
+        print("El arma recargada tiene "+str(cargador)+" balas")
