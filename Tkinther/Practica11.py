3d83bf91b1f068760230e4891a2305d1db8a77da
YaritziLeon <yaritzileonm08@gmail.com>
2023-03-01 15:00:59 -0600

    Funcionalidades Botones, Mensajes

--- Tkinther/Practica11.py
+++ Tkinther/Practica11.py
@@ -1,4 +1,17 @@
-from tkinter import Tk, Button, Frame
+from tkinter import Tk, Button, Frame, messagebox
+
+#5. Agregar funcion de mensaje
+def mostrarMensaje():
+    messagebox.showinfo("Informacion", "Te informo que todo fallo con exito")
+    messagebox.showerror("Error", "Fallo")
+    print(messagebox.askokcancel("Pregunta", "¿Seguro que quieres guardar algo?"))
+    print(messagebox.askyesno("Pregunta","¿Seguro que quieres salir?"))
+
+#6.Funcion agregar botones
+def agregarBoton():
+    botonVerde.config(text="+", bg="green", fg="white")
+    botonNuevo=Button(seccion3, text="Boton Nuevo")
+    botonNuevo.pack()
 
 #1. Generar una ventana 
 ventana=Tk()
@@ -6,27 +19,29 @@ ventana.title("Practica 11")
 ventana.geometry("600x400")
 
 #2. Agregar Frames
-seccion1=Frame(ventana, bg="red")
+seccion1=Frame(ventana, bg="#fcbbfb")
 seccion1.pack(expand=True,fill='both')
 
-seccion2=Frame(ventana, bg="gray")
+seccion2=Frame(ventana, bg="#c3c6c7")
 seccion2.pack(expand=True,fill='both')
 
-seccion3=Frame(ventana, bg="purple")
+seccion3=Frame(ventana, bg="#d79ef7")
 seccion3.pack(expand=True,fill='both')
 
 #3. Agregamos botones
-botonAzul= Button(seccion1,text="boton azul", bg="white", fg="blue")
+botonAzul= Button(seccion1,text="Boton Azul", bg="#b0f5e8", fg="black", command=mostrarMensaje)
 botonAzul.place(x=60,y=60, width=100, height=30)
 
-botonNegro= Button(seccion2,text="boton negro", bg="white", fg="black")
+botonNegro= Button(seccion2,text="Boton Negro", bg="black", fg="white")
 botonNegro.grid(row=0, column=0)
 
-botonRosa= Button(seccion2,text="boton rosa", bg="black", fg="pink")
+botonRosa= Button(seccion2,text="Boton Rosa", bg="#fac3f6", fg="black")
 botonRosa.grid(row=1, column=1)
 
-botonVerde=Button(seccion3,text="boton verde", bg="white", fg="green")
+botonVerde=Button(seccion3,text="Boton Verde", bg="#39fa60", fg="black",command=agregarBoton)
 botonVerde.pack()
+
+#4.Posicionamiento 
 
 #Metodo main para la ejecucion de la ventana
 ventana.mainloop()
