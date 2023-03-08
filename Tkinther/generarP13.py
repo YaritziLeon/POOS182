e0888dcc5f67d9d92cf110d9d7cebb0432361430
YaritziLeon <yaritzileonm08@gmail.com>
2023-03-08 15:41:01 -0600

    clase de la practica 13

---
+++ Tkinther/generarP13.py
@@ -0,0 +1,10 @@
+from tkinter import messagebox
+from random import choice
+
+class generar:
+    def __init__(self):
+        self.__longitud
+        self.__mayusculas
+        self.__caracteres
+        
+    def 
