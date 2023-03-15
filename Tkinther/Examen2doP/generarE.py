647684e80c1110ec455645ad0113081b3c4a740b
YaritziLeon <yaritzileonm08@gmail.com>
2023-03-15 15:31:10 -0600

    Examen2doP

--- Tkinther/Examen2doP/generarE.py
+++ Tkinther/Examen2doP/generarE.py
@@ -3,10 +3,38 @@ from random import choice
 
 class generar:
     def __init__(self,nom,ap,am,a,c):
-        self.nombre=nom
-        self.apellidoP=ap
-        self.apellidoM=am
-        self.año=a
-        self.carrera=c
-    def matricula(self,nom,ap,am,a,c):
+        self.__nombre=nom
+        self.__apellidoP=ap
+        self.__apellidoM=am
+        self.__año=a
+        self.__carrera=c
+    
+    def matricula(self):
         
+        
+    
+    
+    
+    
+    
+    
+    def getNombre(self):
+        return self.__nombre
+    def setNombre(self,nom):
+        self.__nombre=nom
+    def getApellidoP(self):
+        return self.__apellidoP
+    def setApellidoP(self,ap):
+        self.__apellidoP=ap
+    def getApellidoM(self):
+        return self.__apellidoM
+    def setApellidoM(self,am):
+        self.__apellidoM=am
+    def getAño(self):
+        return self.__año
+    def setAño(self,a):
+        self.__año=a
+    def getCarrera(self):
+        return self.__carrera
+    def setCarrera(self,c):
+        self.__carrera=c
