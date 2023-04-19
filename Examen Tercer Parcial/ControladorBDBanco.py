c260f0ba9a70ec9966464ea96314c762939cb7d6
YaritziLeon <yaritzileonm08@gmail.com>
2023-04-19 15:36:37 -0600

    Final de Examen

--- Examen Tercer Parcial/ControladorBDBanco.py
+++ Examen Tercer Parcial/ControladorBDBanco.py
@@ -27,3 +27,35 @@ class controladorBDBanco:
             conex.commit()
             conex.close()
             messagebox.showinfo("Correcto","Usuario guardado")
+            
+    
+    def actualizarCuenta(self,num2,sal2,nom2,cor2,id):
+        
+        conex=self.conexionBD()
+        if(num2 =="" or sal2 =="" or nom2 =="" or cor2 =="" or id ==""):
+            messagebox.showwarning("Error","Llena el Formulario")
+            conex.close()
+        else:
+            try:
+                cursor2=conex.cursor()
+                
+                sqlActualizar="update TBCuentas set NoCuenta=?,Saldo=?,Nombre=?,Correo=? where IDCuenta= "+id
+                datos2=(num2,sal2,nom2,cor2)
+                cursor2.execute(sqlActualizar,datos2)
+                conex.commit()
+                conex.close()
+            except sqlite3.OperationalError:
+                print("Error")
+    
+    
+    def consultarCuentas(self):
+        conex=self.conexBD()
+        try:
+            cursor3=conex.cursor()
+            sqlConsultar="select * from TBCuentas"
+            cursor3.execute(sqlConsultar)
+            Con=cursor3.fetchall()
+            conex.close()
+            return Con
+        except sqlite3.OperationalError:
+            print("Error")
--- Examen Tercer Parcial/Cuentas.py
+++ Examen Tercer Parcial/Cuentas.py
@@ -1,11 +1,25 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+from controladorBDBanco import *
 
+controlador=controladorBDBanco()
 
+def ejecutaInsertar():
+    controlador.InsertarCuenta(varNCuenta.get(),varSaldo.get(),varNombre.get(),varCorreo.get())
 
+def ejecutaActualizar():
+    controlador.actualizarCuenta(varNCuenta.get(),varSaldo.get(),varNombre.get(),varCorreo.get())
+    messagebox.showinfo("Listo","Se actualizo")
 
-
+def ejecutaConsultar():
+    con=controlador.consultarCuentas()
+    tabla.delete(*tabla.get_children())
+    for cuentas in con:
+        cadena1=str(cuentas[0])+" "+str(cuentas[1])+" "+str(cuentas[2])+" "+str(cuentas[3])
+        
+        tabla.insert('',tk.END,text=cuentas[0],values=cadena1)
+        tabla.bind('<<TreeviewSelect>>',ejecutaConsultar)
 
 ventana=Tk()
 ventana.title("Banco")
@@ -18,6 +32,7 @@ pestaña1=ttk.Frame(panel)
 pestaña2=ttk.Frame(panel)
 pestaña3=ttk.Frame(panel)
 
+#1
 titulo1=Label(pestaña1,text="Insertar Cuenta", fg="#95aaf0",font=("ALGERIAN",16)).pack()
 varNCuenta=tk.StringVar()
 lb1NCuenta=Label(pestaña1,text="No de Cuenta: ").pack()
@@ -31,7 +46,46 @@ txtNombre=Entry(pestaña1,textvariable=varNombre).pack()
 varCorreo=tk.StringVar()
 lb1Correo=Label(pestaña1,text="Correo: ").pack()
 txtCorreo=Entry(pestaña1,textvariable=varCorreo).pack()
-btnInsertar=Button(pestaña1,text="Insertar",bg="#b0bde8").pack()
+btnInsertar=Button(pestaña1,text="Insertar",bg="#b0bde8", command=ejecutaInsertar).pack()
+
+#2
+titulo2=Label(pestaña2,text="Actualizar Cuenta", fg="#95aaf0",font=("ALGERIAN",16)).pack()
+varActualizar=tk.StringVar()
+lblid=Label(pestaña2,text="Id de la Cuenta: ").pack()
+txtid=Entry(pestaña2,textvariable=varActualizar).pack()
+
+varNCuenta2=tk.StringVar()
+lb1NCuenta2=Label(pestaña2,text="No de Cuenta: ").pack()
+txtNCuenta2=Entry(pestaña2,textvariable=varNCuenta2).pack()
+varSaldo2=tk.StringVar()
+lb1Saldo2=Label(pestaña2,text="Saldo: ").pack()
+txtSaldo2=Entry(pestaña2,textvariable=varSaldo2).pack()
+varNombre2=tk.StringVar()
+lb1Nombre2=Label(pestaña2,text="Nombre: ").pack()
+txtNombre2=Entry(pestaña2,textvariable=varNombre2).pack()
+varCorreo2=tk.StringVar()
+lb1Correo2=Label(pestaña2,text="Correo: ").pack()
+txtCorreo2=Entry(pestaña2,textvariable=varCorreo2).pack()
+btnACtualizar=Button(pestaña2,text="Actualizar",bg="#b0bde8", command=ejecutaActualizar).pack()
+
+
+#3
+titulo3=Label(pestaña3,text="Consultar Cuentas", fg="#95aaf0",font=("ALGERIAN",16)).pack()
+btnConsultar=Button(pestaña3,text="Consultar",bg="#b0bde8", command=ejecutaConsultar).pack()
+
+subConsultar=Label(pestaña3,text="Cuentas",fg="#f799c7",font=("Modern",12)).pack()
+textConsultar=tk.Text(pestaña3,height=10,width=60)
+textConsultar.pack()
+
+column=('id','num','sal','nom','cor')
+tabla=ttk.Treeview(textConsultar,columns=column,show='headings')
+tabla.heading('id',text='ID')
+tabla.heading('num',text='No de Cuenta')
+tabla.heading('sal',text='Saldo')
+tabla.heading('nom',text='Nombre')
+tabla.heading('cor',text='Correo')
+tabla.pack()
+
 
 panel.add(pestaña1,text="Insertar Cuenta")
 panel.add(pestaña2,text="Actualizar Cuenta")
