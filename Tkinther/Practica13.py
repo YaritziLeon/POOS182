d2db68c0e174b11cce97d79d35efa2f647487f89
YaritziLeon <yaritzileonm08@gmail.com>
2023-04-24 00:41:36 -0600

    Practica 13

--- Tkinther/Practica13.py
+++ Tkinther/Practica13.py
@@ -1,29 +1,40 @@
 from tkinter import Tk, Button, Frame, messagebox
 import tkinter as tk
+from tkinter import *
+from generarP13 import *
+
+generar=generarP13()
+
+def GenerarContraseña():
+    generar.Contraseña(int(varLongitud.get()),varMayusculas.get(),varCaracteres.get())
 
 ventana=Tk()
 ventana.title("Password")
-ventana.geometry("450x150")
+ventana.geometry("450x200")
 
 seccion=Frame(ventana, bg="#fcbbfb")
 seccion.pack(expand=True,fill='both')
 
+varLongitud=tk.StringVar()
 longitud=tk.Label(seccion, text="Ingresa la longitud de tu contraseña: ")
 longitud.place(x=20, y=20)
-longitud1=tk.Entry(seccion, width=30)
+longitud1=tk.Entry(seccion, width=30, textvariable=varLongitud)
 longitud1.place(x=225, y=20)
 
+varMayusculas=tk.StringVar()
 mayusculas=tk.Label(seccion, text="¿Deseas incluir mayusculas?: ")
 mayusculas.place(x=20, y=50)
-mayusculas1=tk.Entry(seccion, width=30)
+mayusculas1=tk.Entry(seccion, width=30, textvariable=varMayusculas)
 mayusculas1.place(x=185, y=50)
 
+varCaracteres=tk.StringVar()
 caracteres=tk.Label(seccion, text="¿Deseas incluir caracteres especiales?: ")
 caracteres.place(x=20, y=80)
-caracteres1=tk.Entry(seccion, width=30)
-caracteres1.place(x=260, y=80)
+caracteres1=tk.Entry(seccion, width=30, textvariable=varCaracteres)
+caracteres1.place(x=230, y=80)
 
-botonGenerar= Button(seccion,text="Ingresar", bg="#b0f5e8", fg="black")
-botonGenerar.place(x=180,y=110, width=100, height=30)
+botonGenerarValidar= Button(seccion,text="Generar y Validar", bg="#b0f5e8", fg="black", command=GenerarContraseña)
+botonGenerarValidar.place(x=180,y=110, width=100, height=30)
+
 
 ventana.mainloop()
--- Tkinther/generarP13.py
+++ Tkinther/generarP13.py
@@ -1,15 +1,51 @@
-from tkinter import messagebox
-from random import choice
+from tkinter import Tk, Button, Frame, messagebox
+import random
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 
-class generar:
-    def __init__(self):
-        self.__longitud
-        self.__mayusculas
-        self.__caracteres
+class generarP13:
         
-    contra = ('0123456789abcdefghijklmnopqrstuvwxyz')
-    mayus = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    carac = ('!"#$%&/()=?¡')
-    
-    def validarContraseña(self,contra,mayus,carac):
+    def Contraseña(self,lon,mayus,carac):
         
+        if (mayus== "no" and carac== "si" and lon >= 8):
+            media1="0123456789abcdefghijklmnopqrstuvwxyz<=>@#%&+"
+            contra1=[random.choice(media1) for i in range(lon)]
+            messagebox.showinfo("Su contraseña no es tan segura: ",contra1)
+            ven1=Tk()
+            txtContra1=Entry(ven1)
+            txtContra1.insert(0,contra1)
+            txtContra1.pack()
+            ven1.mainloop()
+            
+        elif (mayus== "si" and carac== "no" and lon >= 8):
+            media2="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            contra2= [random.choice(media2) for i in range(lon)]
+            messagebox.showinfo("Su contraseña no es tan segura: ",contra2)
+            ven2=Tk()
+            txtContra2=Entry(ven2)
+            txtContra2.insert(0,contra2)
+            txtContra2.pack()
+            ven2.mainloop()
+            
+        elif (mayus== "si" and carac== "si" and lon >= 8):
+            segura="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ<=>@#%&+"
+            contra3= [random.choice(segura) for i in range(lon)]
+            messagebox.showinfo("Su contraseña si es segura: ",contra3)
+            ven3=Tk()
+            txtContra3=Entry(ven3)
+            txtContra3.insert(0,contra3)
+            txtContra3.pack()
+            ven3.mainloop()
+            
+        elif (mayus == "no" and carac == "no" and lon < 8):
+            nsegura="0123456789abcdefghijklmnopqrstuvwxyz"
+            contra4= [random.choice(nsegura) for i in range(lon)]
+            messagebox.showinfo("Su contraseña no es segura: ",nsegura)
+            ven4=Tk()
+            txtContra4=Entry(ven4)
+            txtContra4.insert(0,contra4)
+            txtContra4.pack()
+            ven4.mainloop()
+        else:
+            messagebox.showinfo("Error","La longitud de la contraseña debe igual o mayor a 8")
